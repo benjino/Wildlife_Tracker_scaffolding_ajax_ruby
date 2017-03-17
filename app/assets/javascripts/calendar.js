dd869f5ac7371ab6b0bf143784b9f76831640a0d
kaztateiwa <kaz.t@verizon.net>
2017-03-17 16:59:35 -0700

    Third Commit-FullCalendar

--- app/assets/javascripts/calendar.js
+++ app/assets/javascripts/calendar.js
@@ -1,5 +1,6 @@
 $(document).ready(function() {
    $("#calendar").fullCalendar({
+     eventColor: '#4CAF50',
      events: "/sightings/get_sightings",
      timeFormat: "LT",
      eventClick: function(sightings) {
